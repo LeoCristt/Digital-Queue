e8e8c33638a6332d0405e1afad750d216ae4ec1b
LeoCristt <SergeySav009@yandex.ru>
2025-02-22 13:26:30 +0300

    Initial Project

---
+++ frontend/src/app/queue/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+        <div
+            className="bg-background flex items-center justify-items-center gap-16 h-[885px]">
+            <div className="LeftSide bg-[#16253B] rounded-[6px] flex items-center justify-center w-full h-full">
+                dsa
+            </div>
+            <div className="RightSide bg-[#16253B]  rounded-[6px] flex items-center justify-center w-full h-full   ">
+                da
+            </div>
+
+        </div>
+    );
+}
+
