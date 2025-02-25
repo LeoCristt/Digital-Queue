72b2742655e61e418a11619f967d0b7b4184a77d
Eaglet12IQ <eaglet12iq@gmail.com>
2025-02-25 14:21:38 +0300

    id instead of username in tokens | improved implementation of connection and queuing (in theory)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.api.routers import api_router
 from app.middleware.auto_refresh import auto_refresh_token_middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="My Project")
 
 app.middleware("http")(auto_refresh_token_middleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить все домены (настройте правильно для продакшена!)
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 
 app.include_router(api_router, prefix="/api")
