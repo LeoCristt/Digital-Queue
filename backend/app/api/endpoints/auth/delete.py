822ba779ad74307283b57995247415b0b973c4f1
Eaglet12IQ <eaglet12iq@gmail.com>
2025-03-04 15:08:31 +0300

    delete account

---
+++ backend/app/api/endpoints/auth/delete.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends, HTTPException, Response, Request
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.models.user import User
+from app.core.security import (
+    SECRET_KEY,
+    ALGORITHM
+)
+from jose import jwt
+
+router = APIRouter()
+
+@router.post("/delete")
+async def delete(response: Response, request: Request, db: Session = Depends(get_db)):
+
+    auth_header = request.headers.get("New-Access-Token")
+    if not auth_header:
+        auth_header = request.headers.get("Authorization")
+    
+    access_token = auth_header.split(" ")[1]
+
+    payload = jwt.decode(
+        access_token,
+        SECRET_KEY,
+        algorithms=[ALGORITHM]
+    )
+
+    access_user_id = payload.get("sub")
+    
+    user = db.query(User).filter(User.id == int(access_user_id)).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="Пользователь не найден с таким ID!")
+    
+    response.delete_cookie(
+        key="refresh_token",
+        secure=True,  # Обязательно, если кука устанавливалась с secure=True
+        httponly=True,  # Если использовалось при установке
+    )
+
+    db.delete(user)
+    db.commit()
+    db.refresh(user)
+
+    return {
+        "message": "Пользователь удален.",
+    }
+    
